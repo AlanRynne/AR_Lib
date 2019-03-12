6aaad89dd8f03ba61399c8deef6468301734a2ee
Alan Rynne <alanrynnevidal@me.com>
2019-03-12 18:43:34 +0100

    Added OFFWritter + Commented out rhinoIO methods

--- IO/IO.cs
+++ IO/IO.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Collections;
 using AR_Lib.Geometry;
 using AR_Lib.HalfEdgeMesh;
 
@@ -13,7 +14,7 @@ namespace AR_Lib.IO
     public static class CSVWritter { }
 
     /// OFF format
-    public static class OFFReader 
+    public static class OFFReader
     {
         public static OFFResult ReadMeshFromFile(string FilePath, out OFFMeshData data)
         {
@@ -41,21 +42,21 @@ namespace AR_Lib.IO
             {
                 if (i < (nVertex + start))
                 { // Extract vertices
-                    
+
                     List<double> coords = new List<double>();
                     string[] pointStrings = lines[i].Split(' ');
                     // Iterate over the string fragments and convert them to numbers
-                    foreach(string ptStr in pointStrings)
+                    foreach (string ptStr in pointStrings)
                     {
                         double ptCoord;
-                        if (!Double.TryParse(pointStrings[0], out ptCoord)) return OFFResult.Incorrect_Vertex;
+                        if (!Double.TryParse(ptStr, out ptCoord)) return OFFResult.Incorrect_Vertex;
                         coords.Add(ptCoord);
                     }
                     vertices.Add(new Point3d(coords[0], coords[1], coords[2]));
                 }
                 else if (i < (nVertex + nFaces + start))
                 { // Extract faces
-                    
+
                     // In OFF, faces come with a first number determining the number of vertices in that face
                     List<int> vertexIndexes = new List<int>();
 
@@ -82,8 +83,45 @@ namespace AR_Lib.IO
         }
     }
 
-    public static class OFFWritter { }
+    public static class OFFWritter
+    {
+        public static OFFResult WriteMeshToFile(HE_Mesh mesh, string filePath)
+        {
+            string[] offLines = new string[mesh.Vertices.Count+mesh.Faces.Count+2];
 
+            string offHead = "OFF";
+            offLines[0] = offHead;
+            string offCount = mesh.Vertices.Count + " " + mesh.Faces.Count + " 0";
+            offLines[1] = offCount;
+
+            int count = 2;
+            foreach (HE_Vertex vertex in mesh.Vertices)
+            {
+                string vText = vertex.X + " " + vertex.Y + " " + vertex.Z;
+                offLines[count] = vText;
+                count++;
+
+            }
+            foreach (HE_Face face in mesh.Faces)
+            {
+                if (!face.isBoundaryLoop())
+                {
+                    List<HE_Vertex> vertices = face.adjacentVertices();
+                    string faceString = vertices.Count.ToString();
+
+                    foreach (HE_Vertex v in face.adjacentVertices())
+                    {
+                        faceString = faceString + " " + v.Index; 
+                    }
+                    offLines[count] = faceString;
+                    count++;
+                }
+            }
+
+            System.IO.File.WriteAllLines(filePath, offLines);
+            return OFFResult.OK;
+        }
+    }
 
     public enum OFFResult
     {
@@ -111,48 +149,88 @@ namespace AR_Lib.IO
         public List<Vector3d> normals;
     }
 
-    public static class OBJReader 
-    { 
-        
+    public static class OBJReader
+    {
+
     }
 
-    public static class OBJWritter 
+    public static class OBJWritter
     {
-        
+
     }
-    
+
     #endregion
 
     #region To/From Applications
 
-    public static class RhinoIO
-    {
-        public static RhinoMeshResult ToRhinoMesh(HE_Mesh mesh)
-        {
-            object tmpMesh = new object();
+    // public static class RhinoIO
+    // {
+    //     public static RhinoMeshResult ToRhinoMesh(HE_Mesh mesh, out Rhino.Geometry.Mesh rhinoMesh)
+    //     {
+    //         //CHECKS FOR NGON MESHES!
+    //         if(mesh.isNgonMesh()) 
+    //         {
+    //             rhinoMesh = null;
+    //             return RhinoMeshResult.Invalid;
+    //         }
 
-            return RhinoMeshResult.OK;
+    //         rhinoMesh = new Rhino.Geometry.Mesh();
 
-        }
+    //         foreach(HE_Vertex vertex in mesh.Vertices)
+    //         {
+    //             rhinoMesh.Vertices.Add(vertex.X,vertex.Y,vertex.Z);
+    //         }
 
-        public static HE_Mesh FromRhinoMesh(object rhinoMesh){ throw new NotImplementedException(); }
+    //         foreach(HE_Face face in mesh.Faces)
+    //         {
+    //             List<HE_Vertex> faceVertices = face.adjacentVertices();
+    //             if(faceVertices.Count == 3) rhinoMesh.Faces.AddFace(new Rhino.Geometry.MeshFace(faceVertices[0].Index,faceVertices[1].Index,faceVertices[2].Index));
+    //             if(faceVertices.Count == 4) rhinoMesh.Faces.AddFace(new Rhino.Geometry.MeshFace(faceVertices[0].Index,faceVertices[1].Index,faceVertices[2].Index,faceVertices[3].Index));
+    //         }
 
-        public enum RhinoMeshResult
-        {
-            OK,
-            Empty,
-            Invalid
-        }
-    }
+    //         return RhinoMeshResult.OK;
+
+    //     }
+
+    //     public static RhinoMeshResult FromRhinoMesh(Rhino.Geometry.Mesh rhinoMesh, out HE_Mesh mesh)
+    //     { 
+    //         List<Point3d> vertices = new List<Point3d>();
+    //         List<List<int>> faceVertexIndexes = new List<List<int>>();
+
+    //         foreach(Rhino.Geometry.Point3d vertex in rhinoMesh.Vertices)
+    //         {
+    //             vertices.Add(new Point3d(vertex.X,vertex.Y,vertex.Z));
+    //         }
+    //         foreach(Rhino.Geometry.MeshNgon face in rhinoMesh.GetNgonAndFacesEnumerable())
+    //         {
+
+    //             List<int> list = new List<int>();
+
+    //             foreach(int i in face.BoundaryVertexIndexList()) list.Add(i);
+
+    //             faceVertexIndexes.Add(list);
+
+    //         }
+
+    //         mesh = new HE_Mesh(vertices,faceVertexIndexes);
+    //         return RhinoMeshResult.OK;
+    //     }
+
+    //     public enum RhinoMeshResult
+    //     {
+    //         OK,
+    //         Empty,
+    //         Invalid
+    //     }
+    // }
 
     public static class DynamoIO
     {
-        public static object ToDynamoMesh(HE_Mesh mesh){ throw new NotImplementedException(); }
+        public static object ToDynamoMesh(HE_Mesh mesh) { throw new NotImplementedException(); }
 
-        public static HE_Mesh FromRhinoMesh(object dynamoMesh){ throw new NotImplementedException(); }
+        public static HE_Mesh FromRhinoMesh(object dynamoMesh) { throw new NotImplementedException(); }
 
     }
-
 
     #endregion
 }
