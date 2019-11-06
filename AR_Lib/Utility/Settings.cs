28e0e2684b95980edb19053ba9292026abe48191
Alan Rynne <alan@rynne.es>
2019-11-06 23:27:45 +0100

    Minor modifications to settings

--- AR_Lib/Utility/Settings.cs
+++ AR_Lib/Utility/Settings.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using Newtonsoft.Json;
 
 namespace AR_Lib
 {
@@ -11,18 +10,16 @@ namespace AR_Lib
     /// </summary>
     public class Settings
     {
-        private static int _maxDecimals = 8;
-        private static double _tolerance = 0.0000001;
 
         /// <summary>
         /// Determines the minimum value allowed when using this library.
         /// </summary>
-        public static double Tolerance => _tolerance;
+        public static double Tolerance { get; private set; } = 0.0000001;
 
         /// <summary>
         /// Determines how many decimals are allowed when using the library.
         /// </summary>
-        public static int MaxDecimals => _maxDecimals;
+        public static int MaxDecimals { get; private set; } = 8;
 
         /// <summary>
         /// Modifies the tolerance and computes the maxDecimals value accordingly.
@@ -30,9 +27,9 @@ namespace AR_Lib
         /// <param name="tolerance">Desired tolerance</param>
         public static void ModifyTolerance(double tolerance)
         {
-            _tolerance = tolerance;
+            Tolerance = tolerance;
             string t = tolerance.ToString("N14");
-            _maxDecimals = t.Substring(t.IndexOf(".") + 1).IndexOf("1");
+            MaxDecimals = t.Substring(t.IndexOf(".") + 1).IndexOf("1");
         }
 
     }
