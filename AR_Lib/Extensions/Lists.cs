fd865d3b283f9d681f3cd2882741ef2903e2b407
Alan Rynne <alan@rynne.es>
2019-11-10 10:55:18 +0100

    Testing codecov.io-travis.ci link Created new Extensions namespace Moved Intersection classes to Intersec folder Deleted old non-working surface Modified Launch.json names

---
+++ AR_Lib/Extensions/Lists.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AR_Lib.Extensions
+{/// <summary>
+ /// Static class holding some utility methods regarding object collections
+ /// </summary>
+    public static class Lists
+    {
+        /// <summary>
+        /// Initializes a new list full of objects initialized with their default constructor.
+        /// </summary>
+        /// <param name="count">Number of objects in the list.</param>
+        /// <typeparam name="T">Type of object in the list.</typeparam>
+        public static List<T> RepeatedDefault<T>(int count)
+        {
+            return Repeated(default(T), count);
+        }
+
+        /// <summary>
+        /// Initializes a new list full of objects initialized the values of the specified instance of T
+        /// </summary>
+        /// <param name="value">Object to insert on every index of the list.</param>
+        /// <param name="count">Number of objects in the list.</param>
+        /// <typeparam name="T">Type of object in the list.</typeparam>
+        public static List<T> Repeated<T>(T value, int count)
+        {
+            List<T> repeated = new List<T>(count);
+            repeated.AddRange(Enumerable.Repeat(value, count));
+            return repeated;
+        }
+    }
+}
