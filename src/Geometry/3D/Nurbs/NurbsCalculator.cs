413dae982b28c209588c1e4e2ca61169b4d950d7
Alan Rynne <alan@rynne.es>
2020-02-17 08:24:14 +0100

    Added more nurbs methods from book

--- src/Geometry/3D/Nurbs/NurbsCalculator.cs
+++ src/Geometry/3D/Nurbs/NurbsCalculator.cs
@@ -10,6 +10,26 @@ namespace Paramdigma.Core.Geometry
     public static class NurbsCalculator
     {
         /// <summary>
+        /// Constructs a Unit knot vector given a point count and degree.
+        /// </summary>
+        /// <param name="controlPointCount">Ammount of control points in the curve.</param>
+        /// <param name="degree">Degree of the curve.</param>
+        /// <returns></returns>
+        public static double[] CreateUnitKnotVector(int controlPointCount, int degree)
+        {
+            if (degree > controlPointCount)
+                throw new Exception("Degree cannot be bigger than 'ControlPoints - 1'");
+            var knotVector = new double[controlPointCount + degree + 2];
+            for (int i = 0; i <= degree; i++)
+                knotVector[i] = 0.0;
+            for (int i = degree + 1; i < controlPointCount + 1; i++)
+                knotVector[i] = ((double)i - degree) / (controlPointCount - degree + 1);
+            for (int i = controlPointCount + 1; i < controlPointCount + degree + 2; i++)
+                knotVector[i] = 1.0;
+            return knotVector;
+        }
+
+        /// <summary>
         /// Compute a point on a power basis curve.
         /// </summary>
         /// <param name="points">Curve points.</param>
@@ -556,6 +576,7 @@ namespace Paramdigma.Core.Geometry
             return ck;
         }
 
+        // B-Spline Surfaces
         public static Point3d SurfacePoint(int n, int p, IList<double> knotVectorU, int m, int q, IList<double> knotVectorV, Matrix<Point3d> controlPoints, double u, double v)
         {
             var uspan = FindSpan(n, p, u, knotVectorU);
@@ -576,24 +597,211 @@ namespace Paramdigma.Core.Geometry
             return surfPt;
         }
 
-        /// <summary>
-        /// Constructs a Unit knot vector given a point count and degree.
-        /// </summary>
-        /// <param name="controlPointCount">Ammount of control points in the curve.</param>
-        /// <param name="degree">Degree of the curve.</param>
-        /// <returns></returns>
-        public static double[] CreateUnitKnotVector(int controlPointCount, int degree)
+        public static Point3d[,] SurfaceDerivsAlg1(int n, int p, IList<double> knotVectorU, int m, int q, IList<double> knotVectorV, Matrix<Point3d> controlPoints, double u, double v, int derivCount)
         {
-            if (degree > controlPointCount)
-                throw new Exception("Degree cannot be bigger than 'ControlPoints - 1'");
-            var knotVector = new double[controlPointCount + degree + 2];
-            for (int i = 0; i <= degree; i++)
-                knotVector[i] = 0.0;
-            for (int i = degree + 1; i < controlPointCount + 1; i++)
-                knotVector[i] = ((double)i - degree) / (controlPointCount - degree + 1);
-            for (int i = controlPointCount + 1; i < controlPointCount + degree + 2; i++)
-                knotVector[i] = 1.0;
-            return knotVector;
+            var sKL = new Point3d[derivCount + 1, derivCount + 1];
+            var du = Math.Min(derivCount, p);
+            for (var k = p + 1; k <= derivCount; k++)
+            {
+                for (var l = 0; l <= derivCount - k; l++)
+                {
+                    sKL[k, l] = Point3d.Unset;
+                }
+            }
+
+            var dv = Math.Min(derivCount, q);
+            for (var l = q + 1; l <= derivCount; l++)
+            {
+                for (var k = 0; k <= derivCount - 1; k++)
+                {
+                    sKL[k, l] = Point3d.Unset;
+                }
+            }
+
+            var uSpan = FindSpan(n, p, u, knotVectorU);
+            var nU = DersBasisFuns(uSpan, u, p, du, knotVectorU);
+            var vSpan = FindSpan(m, q, v, knotVectorV);
+            var nV = DersBasisFuns(vSpan, v, q, dv, knotVectorV);
+
+            for (var k = 0; k <= du; k++)
+            {
+                var temp = new Point3d[q];
+                for (var s = 0; s <= q; s++)
+                {
+                    temp[s] = Point3d.Unset;
+                    for (var r = 0; r <= p; r++)
+                    {
+                        temp[s] += nU[k, r] * controlPoints[uSpan - p + r, vSpan - q + s];
+                    }
+
+                    var dd = Math.Min(derivCount - k, dv);
+
+                    for (var l = 0; l <= dd; l++)
+                    {
+                        sKL[k, l] = Point3d.Unset;
+
+                        // TODO: Check ss, this was changed from 's' for naming conflicts but it might have been on purpose.
+                        for (var ss = 0; ss <= q; ss++)
+                        {
+                            sKL[k, l] += nV[l, ss] * temp[ss];
+                        }
+                    }
+                }
+            }
+
+            return sKL;
+        }
+
+        public static Point3d[][][][] SurfaceDerivCpts(int n, int p, IList<double> knotVectorU, int m, int q, IList<double> knotVectorV, Matrix<Point3d> controlPoints, int d, int r1, int r2, int s1, int s2)
+        {
+            var pkl = new Point3d[d][][][];
+
+            var du = Math.Min(d, p);
+            var dv = Math.Min(d, q);
+            var r = r2 - r1;
+            var s = s2 - s1;
+
+            for (var j = s1; j <= s2; j++)
+            {
+                var temp = CurveDerivCpts(n, p, knotVectorU, controlPoints.Column(j), du, r1, r2);
+                for (var k = 0; k <= du; k++)
+                {
+                    for (int i = 0; i <= r - k; i++)
+                    {
+                        pkl[k][0][i][j - s1] = temp[k, i];
+                    }
+                }
+            }
+
+            for (var k = 0; k <= du; k++)
+            {
+                for (var i = 0; i <= r - k; i++)
+                {
+                    var dd = Math.Min(d - k, dv);
+                    var temp = CurveDerivCpts(m, q, knotVectorV, pkl[k][0][i], dd, 0, s);
+                    for (var l = i; k <= dd; k++)
+                    {
+                        for (var j = 0; j <= s - l; j++)
+                        {
+                            pkl[k][l][i][j] = temp[l, j];
+                        }
+                    }
+                }
+            }
+
+            return pkl;
+        }
+
+        public static Point3d[,] SurfaceDerivsAlg2(int n, int p, IList<double> knotVectorU, int m, int q, IList<double> knotVectorV, Matrix<Point3d> controlPoints, double u, double v, int d)
+        {
+            var skl = new Point3d[d + 1, d + 1];
+
+            var du = Math.Min(d, p);
+            for (var k = p + 1; k <= d; k++)
+            {
+                for (var l = 0; l <= d - k; l++)
+                {
+                    skl[k, l] = Point3d.Unset;
+                }
+            }
+
+            var dv = Math.Min(d, q);
+            for (var l = q + 1; l <= d; l++)
+            {
+                for (var k = 0; k <= d - l; k++)
+                {
+                    skl[k, l] = Point3d.Unset;
+                }
+            }
+
+            var uSpan = FindSpan(n, p, u, knotVectorU);
+            var nV = AllBasisFuns(uSpan, u, p, knotVectorU);
+            var vSpan = FindSpan(m, q, v, knotVectorV);
+            var nU = AllBasisFuns(vSpan, v, q, knotVectorV);
+            var pkl = SurfaceDerivCpts(n, p, knotVectorU, m, q, knotVectorV, controlPoints, d, uSpan - p, uSpan, vSpan - q, vSpan);
+
+            for (var k = 0; k <= du; k++)
+            {
+                var dd = Math.Min(d - k, dv);
+                for (var l = 0; l <= dd; l++)
+                {
+                    skl[k, l] = Point3d.Unset;
+                    for (var i = 0; i <= q - l; i++)
+                    {
+                        var tmp = Point3d.Unset;
+                        for (var j = 0; j <= p - k; j++)
+                            tmp += nV[j, p - k] * pkl[k][l][j][i];
+                        skl[k, l] += nU[i, q - l] * tmp;
+                    }
+                }
+            }
+
+            return skl;
+        }
+
+        // Nubs methods
+        public static Point4d CurvePoint(int n, int p, IList<double> U, IList<Point4d> Pw, double u)
+        {
+            var span = FindSpan(n, p, u, U);
+            var N = BasisFuns(span, u, p, U);
+            var Cw = new Point4d();
+            for (int j = 0; j <= p; j++)
+            {
+                Cw += N[j] * Pw[span - p + j];
+            }
+
+            return Cw / Cw.Weight;
+        }
+
+        /// <summary>
+        /// Compute C(u) derivatives from Cw(u) derivatives.
+        /// </summary>
+        /// <param name="aDers"></param>
+        /// <param name="wDers"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static Vector3d[] RatCurveDerivs(IList<Vector3d> aDers, IList<double> wDers, int d)
+        {
+            var ders = new Vector3d[d + 1];
+            double[,] bin = null; // TODO: Precompute 'binomial coefficients'
+            for (int k = 0; k <= d; k++)
+            {
+                var v = aDers[k];
+                for (int i = 1; i <= k; i++)
+                {
+                    v -= bin[k, i] * wDers[i] * ders[k - i];
+                }
+
+                ders[k] = v / wDers[0];
+            }
+
+            return ders;
+        }
+
+        public static Point3d SurfacePoint(int n, int p, IList<double> U, int m, int q, IList<double> V, Matrix<Point4d> Pw, double u, double v)
+        {
+            var uspan = FindSpan(n, p, u, U);
+            var nU = BasisFuns(uspan, u, p, U);
+            var vspan = FindSpan(m, q, v, V);
+            var nV = BasisFuns(vspan, v, q, V);
+
+            var temp = new Point4d[q];
+            for (var l = 0; l <= q; l++)
+            {
+                temp[l] = new Point4d();
+                for (var k = 0; k <= p; k++)
+                {
+                    temp[l] += nU[k] * Pw[uspan - p + k, vspan - q + l];
+                }
+            }
+
+            var sW = new Point4d();
+            for (var l = 0; l <= q; l++)
+            {
+                sW += nV[l] * temp[l];
+            }
+
+            return (Point3d)(sW / sW.Weight);
         }
     }
 }
